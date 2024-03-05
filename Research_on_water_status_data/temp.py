b4fd0d1f15c612ba22b454347f359d9261243674
Yiyu Zhong <zhong_yi_yu@outlook.com>
2024-03-05 15:50:45 +0800

    Update

--- Research_on_water_status_data/temp.py
+++ Research_on_water_status_data/temp.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
 # 读取csv文件
-df = pd.read_csv('airside_waterflow_210A.csv')
+df = pd.read_csv('hydrogenside_waterflow_300A_in_ratio.csv')
 
 # 根据第一列和第二列排序
-df_sorted = df.sort_values(by=['ratio', 'temperature'])
+df_sorted = df.sort_values(by=['temperature','ratio'])
 
 # 打印排序后的DataFrame
 print(df_sorted)
+
+df_sorted = df_sorted[['temperature','ratio','waterflow']]
 
 df_sorted.to_csv("output.csv",index=False)
